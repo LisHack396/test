66e7d62d7aa2d33e379ab01365fb324d8ac57e01
LisHack396 <lisandrav2003@gmail.com>
2024-08-05 09:49:38 -0400

    update chatbot

--- scripts/chatbot.py
+++ scripts/chatbot.py
@@ -1,5 +1,5 @@
 import os
-import dotenv
+from dotenv import load_dotenv
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
 from langchain.schema import (AIMessage, HumanMessage)
